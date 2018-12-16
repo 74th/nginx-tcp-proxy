c60943a71cc15185987aeda92d4d3346bea0b4c4
74th <site@j74th.com>
2018-12-16 17:03:49 +0900

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# nginx TCP Stream Proxy
+
+## how to use
+
+```
+docker run -it --rm -e HOST=192.168.1.150 -e PORT=6379 -p 6379:6379 74th:nginx-proxy
+redis-cli
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-cat /etc/nginx/nginx.conf | sed -e "s/__HOST__/$HOST/g" | sed -e "s/__PORT__/$PORT/g" > /etc/nginx/nginx.conf
+sed -i -e s/__HOST__/$HOST/g -e s/__PORT__/$PORT/g /etc/nginx/nginx.conf
 nginx -g "daemon off;"
